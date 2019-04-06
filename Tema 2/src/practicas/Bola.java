36b56a9ded52c554039679d9316d4692c421a295
silvagomez <silva.camilo@gmail.com>
2019-04-06 23:54:00 +0200

    prácticas

--- Tema 2/src/practicas/Bola.java
+++ Tema 2/src/practicas/Bola.java
@@ -1,4 +1,4 @@
-package ejercicios;
+package practicas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
--- Tema 2/src/practicas/FloatMayor.java
+++ Tema 2/src/practicas/FloatMayor.java
@@ -1,4 +1,4 @@
-package ejercicios;
+package practicas;
 import java.util.Scanner;
 
 public class FloatMayor {
